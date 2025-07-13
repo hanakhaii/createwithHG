7091af3a310c701e52769c4c10fbe1675c6ddb19
hana khyy <emailbaru@example.com>
2025-07-13 21:14:55 +0700

    percobaan

--- wp-content/themes/createwithHG/project.php
+++ wp-content/themes/createwithHG/project.php
@@ -1,16 +1,20 @@
-<?php 
-
+<?php
+/*
+Template Name: Project Page
+*/
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <a href="<?php echo get_template_directory_uri(); ?>/project.php">here</a>
+    <h1>hana love galuh forever</h1>
+    
+    <a href="/">Back</a>
 </body>
 </html>
